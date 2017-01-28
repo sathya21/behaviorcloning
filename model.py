6ba5e67b1833f1151429b7fcf68fe753a797be72
sathya21 <sathya21@gmail.com>
2017-01-28 22:52:27 +0530

    farther lake

--- model.py
+++ model.py
@@ -52,23 +52,23 @@ def get_model(time_len=1):
 
 
   model.add(Flatten())
-  model.add(Dense(512))
-  model.add(Dropout(.2))
+  model.add(Dense(1164, init='he_normal'))
+  #model.add(Dropout(.2))
   model.add(ELU())
 
-  model.add(Dense(100))
-  model.add(Dropout(.3))
-
-  model.add(ELU())
-  model.add(Dense(50))
-  model.add(Dropout(.3))
-
-  model.add(ELU())
-  model.add(Dense(10))
+  model.add(Dense(100, init='he_normal'))
   #model.add(Dropout(.3))
 
   model.add(ELU())
-  model.add(Dense(1))
+  model.add(Dense(50, init='he_normal'))
+  #model.add(Dropout(.3))
+
+  model.add(ELU())
+  model.add(Dense(10, init='he_normal'))
+  #model.add(Dropout(.3))
+
+  model.add(ELU())
+  model.add(Dense(1, init='he_normal'))
 
   adam= Adam(lr=0.0001)
   #, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0
@@ -93,12 +93,12 @@ def read_csv():
               image_resized = cv2.resize(img1, (200, 100))
               crop_img = image_resized[34:100,0:200 ]
               X_train_list.append(crop_img)
-              y_train_list.append(float(i[3])+0.27)
+              y_train_list.append(float(i[3])+0.15)
               img1 = mpimg.imread(i[2].strip())
               image_resized = cv2.resize(img1, (200, 100))
               crop_img = image_resized[34:100,0:200 ]
               X_train_list.append(crop_img)
-              y_train_list.append(float(i[3])-0.27)
+              y_train_list.append(float(i[3])-0.15)
           j=j+1
 
 X_train_list = []
@@ -124,7 +124,7 @@ X_train, y_train = shuffle(X_train, y_train)
 
 model = get_model()
 batch_size = 128
-nb_epoch = 7
+nb_epoch = 8
 
 model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_validation, y_validation))
 
